5535c7baf24640692cb76fa5dcd3d666f1715dc5
kawsar007 <imkawsar007@gmail.com>
2024-05-27 17:44:02 +0600

    API Setup and Fetch data from contentful api

--- app/page.js
+++ app/page.js
@@ -1,5 +1,31 @@
-const url = `${process.env.BASE_URL}/spaces/${process.env.SPACE_ID}/environments/staging/entries?access_token=${process.env.ACCESS_TOKEN}`;
-console.log(url);
-export default function Home() {
-  return <main>Contentful with Next Js</main>;
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+
+const url = `${process.env.BASE_URL}/spaces/${process.env.SPACE_ID}/environments/master/entries?access_token=${process.env.ACCESS_TOKEN}`;
+
+const options = {
+  renderText: (text) => {
+    return text.split("\n").reduce((children, textSegment, index) => {
+      return [...children, index > 0 && <br key={index} />, textSegment];
+    }, []);
+  },
+};
+
+export default async function Home() {
+  const response = await fetch(url, {
+    cache: "no-store",
+  });
+  const data = await response.json();
+  console.log(data);
+  return (
+    <main>
+      {data.items.map((item, i) => (
+        <div key={i}>
+          <h1 className="text-3xl font-bold px-24">{item.fields?.title}</h1>
+          <div className="py-4 px-24">
+            {documentToReactComponents(item.fields?.body)}
+          </div>
+        </div>
+      ))}
+    </main>
+  );
 }
