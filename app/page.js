b29dab2e0145a1a6e4d4581cf0a066b28bd64013
kawsar007 <imkawsar007@gmail.com>
2024-05-28 15:36:47 +0600

    Image render issue fix

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import Image from "next/image";
 
 const url = `${process.env.BASE_URL}/spaces/${process.env.SPACE_ID}/environments/master/entries?access_token=${process.env.ACCESS_TOKEN}`;
 
@@ -14,18 +15,44 @@ export default async function Home() {
   const response = await fetch(url, {
     cache: "no-store",
   });
+
+  if (!response.ok) {
+    console.error("Failed to fetch data");
+    return <div>Failed to load content.</div>;
+  }
   const data = await response.json();
-  console.log(data);
+
+  if (!data.items || !data.includes) {
+    console.error("Invalid data structure", data);
+    return <div>Invalid content structure</div>;
+  }
   return (
     <main>
-      {data.items.map((item, i) => (
-        <div key={i}>
-          <h1 className="text-3xl font-bold px-24">{item.fields?.title}</h1>
-          <div className="py-4 px-24">
-            {documentToReactComponents(item.fields?.body)}
+      {data.items.map((item, i) => {
+        const image = data.includes.Asset.find(
+          (asset) => asset.sys.id === item?.fields?.image?.sys?.id
+        );
+
+        const imageUrl = image?.fields?.file?.url;
+        const fullImageUrl = imageUrl ? `https:${imageUrl}` : null;
+
+        return (
+          <div key={i}>
+            <h1 className="text-3xl font-bold px-24">{item?.fields?.title}</h1>
+            <div className="py-4 px-24">
+              {documentToReactComponents(item?.fields?.body)}
+            </div>
+            {fullImageUrl && (
+              <Image
+                src={fullImageUrl}
+                width={500}
+                height={500}
+                alt={item?.fields?.title || "blog image"}
+              />
+            )}
           </div>
-        </div>
-      ))}
+        );
+      })}
     </main>
   );
 }
